d8bfe4e449347f408d75857045abe34299d34ac4
alexandreduara <alexandreduara@gmail.com>
2023-01-25 16:03:27 -0300

    Commit piloto

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+function tocaSom (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+
+    // template string
+    const audio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom (audio);
+    }
+    contador = contador + 1;
+}1
