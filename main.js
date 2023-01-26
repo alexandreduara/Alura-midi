5d2c503cd99e924ca95060b4355a6af27744148d
alexandreduara <alexandreduara@gmail.com>
2023-01-26 16:36:37 -0300

    deixando mais limpo o código

--- main.js
+++ main.js
@@ -6,18 +6,29 @@ function tocaSom (idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length); contador++{
 
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1];
 
-    // template string
     const audio = `#som_${instrumento}`;
 
     tecla.onclick = function () {
         tocaSom (audio);
     }
-    contador = contador + 1;
-}1
+}
+
+// while (contador < listaDeTeclas.length){
+
+//     const tecla = listaDeTeclas[contador]
+//     const instrumento = tecla.classList[1];
+
+//     // template string
+//     const audio = `#som_${instrumento}`;
+
+//     tecla.onclick = function () {
+//         tocaSom (audio);
+//     }
+//     contador = contador + 1;
+// }1
